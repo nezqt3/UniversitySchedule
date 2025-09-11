9c3a8de9d7fa482aa4b716d0fc0502d1f730fddc
Denis <dalekseenko8@gmail.com>
2025-09-11 18:23:56 +0300

    Initial Commit

---
+++ UniversitySchedule/UniversityScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityScheduleApp.swift
+//  UniversitySchedule
+//
+//  Created by Денис Алексеенко on 11.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct UniversityScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ UniversityScheduleTests/UniversityScheduleTests.swift
@@ -0,0 +1,17 @@
+//
+//  UniversityScheduleTests.swift
+//  UniversityScheduleTests
+//
+//  Created by Денис Алексеенко on 11.09.2025.
+//
+
+import Testing
+@testable import UniversitySchedule
+
+struct UniversityScheduleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
