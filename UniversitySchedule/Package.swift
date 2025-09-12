89ee80bab9cf8aa773070b6773fcd44e0c31e6ce
Denis <dalekseenko8@gmail.com>
2025-09-12 18:30:00 +0300

    remove package

--- UniversitySchedule/Package.swift
+++
@@ -1,9 +0,0 @@
-dependencies: [
-    .package(url: "https://github.com/scinfu/SwiftSoup.git", from: "2.6.0")
-],
-targets: [
-    .target(
-        name: "YourTarget",
-        dependencies: ["SwiftSoup"]
-    )
-]
