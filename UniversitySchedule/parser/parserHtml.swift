5a84b8312b4202cd1d3e5ac21e1bea94f03e5a07
Denis <dalekseenko8@gmail.com>
2025-09-12 09:59:52 +0300

    change parser

---
+++ UniversitySchedule/parser/parserHtml.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+class parserHtml {
+    
+    func parseHtml(from urlString: String) async throws -> String {
+        let url = URL(string: urlString)
+        return ""
+    }
+
+}
